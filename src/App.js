7dae3bb270a8bdd5de2ecf72e093069c81456e43
Kirill <kirisakov4@yandex.ru>
2022-06-27 17:50:05 +0300

    Create empty react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './scss/app.scss'
+import Header from './components/UI/header'
+import Footer from './components/UI/footer'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>Main</main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
