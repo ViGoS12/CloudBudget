b2da1e97962123ee9d2eab8ff953123787109882
Kirill <kirisakov4@yandex.ru>
2022-06-28 22:59:42 +0300

    Added feature section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Header from './components/UI/header'
 import Footer from './components/UI/footer'
 import video from './assets/img/video_bg.jpg'
 import btn from './assets/svg/triangleR_btn.svg'
+import feature1 from './assets/img/features1.jpg'
+import feature2 from './assets/img/features2.jpg'
+import feature3 from './assets/img/features3.jpg'
 
 function App() {
   return (
@@ -22,7 +25,7 @@ function App() {
       </section>
       <main>
         <section className='info'>
-          <div className='conteiner'>
+          <div className='container'>
             <div className='leftside'>
               <div className='info__title'>
                 Easy to Use Cloud Budget Management Software
@@ -45,8 +48,46 @@ function App() {
           </div>
         </section>
         <section className='features'>
-          <div className='conteiner'>
-            <div className='features__title'>Features</div>
+          <div className='features__title'>Features</div>
+          <div className='container'>
+            <div className='features__block'>
+              <img src={feature1} alt='' className='features__img' />
+              <div className='features__description'>
+                <div className='features__title_desc'>
+                  Supports All Currencies and Cards
+                </div>
+                <div className='features__text'>
+                  We support all popular currencies and is fully customizable to
+                  add
+                </div>
+                <div className='features__link'>Read More</div>
+              </div>
+            </div>
+            <div className='features__block'>
+              <img src={feature2} alt='' className='features__img' />
+              <div className='features__description'>
+                <div className='features__title_desc'>
+                  Manage your expenses on the go
+                </div>
+                <div className='features__text'>
+                  You can access your account from anywhere in the world on any
+                  device
+                </div>
+                <div className='features__link'>Read More</div>
+              </div>
+            </div>
+            <div className='features__block'>
+              <img src={feature3} alt='' className='features__img' />
+              <div className='features__description'>
+                <div className='features__title_desc'>
+                  Cloud Budget Management Software
+                </div>
+                <div className='features__text'>
+                  Our software is made so you can access and manage your budget
+                </div>
+                <div className='features__link'>Read More</div>
+              </div>
+            </div>
           </div>
         </section>
       </main>
